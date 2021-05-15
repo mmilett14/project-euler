79b2b603073ed40ecdf998453d2f6867e86fdaba
Mike <mmilett14@gmail.com>
2021-05-15 11:31:36 -0500

    not working yet

---
+++ 3-largest-prime-factor.py
@@ -0,0 +1,10 @@
+lrg_prime = 0
+
+for i in range (0, 600851475143):
+    if 600851475143 % i == 0 and i % 1 > 0 and i % 2 > 0 and i % 3 > 0 and i % 4 > 0 and i % 5 > 0 and i % 6 > 0 and i % 7 > 0 and i % 8 > 0 and i % 9 > 0:
+        lrg_prime = i
+
+print(lrg_prime)
+
+    
+
