31797451ed60b58d48c5475fed6fd71b3efc2234
Mike <mmilett14@gmail.com>
2021-05-17 15:27:20 -0500

    found solution but code not optimal

--- 3-largest-prime-factor.py
+++ 3-largest-prime-factor.py
@@ -1,10 +1,10 @@
-lrg_prime = 0
+import math
 
-for i in range (0, 600851475143):
-    if 600851475143 % i == 0 and i % 1 > 0 and i % 2 > 0 and i % 3 > 0 and i % 4 > 0 and i % 5 > 0 and i % 6 > 0 and i % 7 > 0 and i % 8 > 0 and i % 9 > 0:
+lrg_prime = 1
+
+euler = 600851475143
+
+for i in range (1, math.sqrt(euler)):
+    if euler % i == 0:
         lrg_prime = i
-
-print(lrg_prime)
-
-    
-
+        print(lrg_prime)
