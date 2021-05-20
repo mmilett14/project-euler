ea544600c4186059ca343d9f91127ee884d1d8f3
mike <78767272+mmilett14@users.noreply.github.com>
2021-05-20 15:37:43 -0500

    solved first part of problem

---
+++ 5-smallest-multiple.py
@@ -0,0 +1,10 @@
+# First part of problem
+
+solved = 0
+i = 1
+
+while solved < 1:
+    if i % 1 == 0 and i % 2 == 0 and i % 3 == 0 and i % 4 == 0 and i % 5 == 0 and i % 6 == 0 and i % 7 == 0 and i % 8 == 0 and i % 9 == 0 and i % 10 == 0:
+        solved = solved + 1
+        print(i)
+    i = i + 1
