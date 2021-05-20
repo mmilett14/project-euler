8cf4911f27b8fcd8ef2d59ebbd1feb21d948a50e
mike <78767272+mmilett14@users.noreply.github.com>
2021-05-20 15:47:31 -0500

    possible solution but seems non-optimal

--- 5-smallest-multiple.py
+++ 5-smallest-multiple.py
@@ -8,3 +8,34 @@ while solved < 1:
         solved = solved + 1
         print(i)
     i = i + 1
+
+# Second part of problem
+
+solved = 0
+i = 1
+
+while solved < 1:
+    if isinstance(i / 1, float) == False and \
+    isinstance(i / 2, float) == False and \
+    isinstance(i / 3, float) == False and \
+    isinstance(i / 4, float) == False and \
+    isinstance(i / 5, float) == False and \
+    isinstance(i / 6, float) == False and \
+    isinstance(i / 7, float) == False and \
+    isinstance(i / 8, float) == False and \
+    isinstance(i / 9, float) == False and \
+    isinstance(i / 10, float) == False and \
+    isinstance(i / 11, float) == False and \
+    isinstance(i / 12, float) == False and \
+    isinstance(i / 13, float) == False and \
+    isinstance(i / 14, float) == False and \
+    isinstance(i / 15, float) == False and \
+    isinstance(i / 16, float) == False and \
+    isinstance(i / 17, float) == False and \
+    isinstance(i / 18, float) == False and \
+    isinstance(i / 19, float) == False and \
+    isinstance(i / 20, float) == False:
+        solved = solved + 1
+        print(i)
+    
+    i = i + 1
