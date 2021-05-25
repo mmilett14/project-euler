f741b22df6bfbcea19f33aba54a62c39703c2ea9
mike <78767272+mmilett14@users.noreply.github.com>
2021-05-24 21:18:00 -0500

    solved

--- 5-smallest-multiple.py
+++ 5-smallest-multiple.py
@@ -1,41 +1,9 @@
-# First part of problem
-
 solved = 0
 i = 1
 
 while solved < 1:
-    if i % 1 == 0 and i % 2 == 0 and i % 3 == 0 and i % 4 == 0 and i % 5 == 0 and i % 6 == 0 and i % 7 == 0 and i % 8 == 0 and i % 9 == 0 and i % 10 == 0:
+    if i % 1 == 0 and i % 2 == 0 and i % 3 == 0 and i % 4 == 0 and i % 5 == 0 and i % 6 == 0 and i % 7 == 0 and i % 8 == 0 and i % 9 == 0 and i % 10 == 0 \
+        and i % 11 == 0 and i % 12 == 0 and i % 13 == 0 and i % 14 == 0 and i % 15 == 0 and i % 16 == 0 and i % 17 == 0 and i % 18 == 0 and i % 19 == 0 and i % 20 == 0:
         solved = solved + 1
         print(i)
-    i = i + 1
-
-# Second part of problem
-
-solved = 0
-i = 1
-
-while solved < 1:
-    if isinstance(i / 1, float) == False and \
-    isinstance(i / 2, float) == False and \
-    isinstance(i / 3, float) == False and \
-    isinstance(i / 4, float) == False and \
-    isinstance(i / 5, float) == False and \
-    isinstance(i / 6, float) == False and \
-    isinstance(i / 7, float) == False and \
-    isinstance(i / 8, float) == False and \
-    isinstance(i / 9, float) == False and \
-    isinstance(i / 10, float) == False and \
-    isinstance(i / 11, float) == False and \
-    isinstance(i / 12, float) == False and \
-    isinstance(i / 13, float) == False and \
-    isinstance(i / 14, float) == False and \
-    isinstance(i / 15, float) == False and \
-    isinstance(i / 16, float) == False and \
-    isinstance(i / 17, float) == False and \
-    isinstance(i / 18, float) == False and \
-    isinstance(i / 19, float) == False and \
-    isinstance(i / 20, float) == False:
-        solved = solved + 1
-        print(i)
-    
     i = i + 1
