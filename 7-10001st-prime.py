bdeafbe9df79dc76401a3512f8d762bd2b1f930d
mmilett14 <mmilett14@gmail.com>
2021-05-26 21:54:42 -0500

    solved first part of problem

--- 7-10001st-prime.py
+++ 7-10001st-prime.py
@@ -1,1 +1,14 @@
 # Implement Sieve of Eratosthenes.
+
+# Solved first part of problem.
+marked = []
+prime = []
+
+for i in range (2,14):
+    if i not in marked:
+        prime.append(i)
+
+        for j in range (2, 14):
+            marked.append(i*j)
+
+print(prime)
