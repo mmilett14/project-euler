3e8ff337e4525e7bfd31e1eed4245740fcb825c0
mike <78767272+mmilett14@users.noreply.github.com>
2021-05-27 14:19:10 -0500

    began work on solution

--- 7-10001st-prime.py
+++ 7-10001st-prime.py
@@ -1,6 +1,4 @@
-# Implement Sieve of Eratosthenes.
-
-# Solved first part of problem.
+# Solved first part of problem using Sieve of Eratosthenes.
 marked = []
 prime = []
 
@@ -12,3 +10,24 @@ for i in range (2,14):
             marked.append(i*j)
 
 print(prime)
+
+# Need to solve second part of problem using Trial Division.
+
+### This needs work.
+
+num_list = [2]
+
+prime = [2]
+
+i = 2
+
+while len(prime) < 10:
+    i = i + 1
+    
+    
+    for num in num_list:
+        if i % num != 0:
+            prime.append(i)
+            print(prime)
+    
+    num_list.append(i)
