2c32563b7559876d5ce6d73d91e1447534f9a112
mike <78767272+mmilett14@users.noreply.github.com>
2021-06-08 13:53:58 -0500

    Solved first part of problem

--- 8-largest-product-in-a-series.py
+++ 8-largest-product-in-a-series.py
@@ -1,1 +1,13 @@
 # Use sliding Window
+
+# Solved first part of problem
+window_max = 0
+i = 0
+
+for i in range(0, len(num)-3):
+    window_prod = int(num[i]) * int(num[i+1]) * int(num[i+2]) * int(num[i+3])
+
+    if window_prod > window_max:
+        window_max = window_prod
+
+print(window_max)
