699e5b32132752a86b027a15521e9f0d4050d5f0
mike <78767272+mmilett14@users.noreply.github.com>
2021-08-24 15:15:33 -0500

    progress on dynamic solution

--- 7-10001st-prime.py
+++ 7-10001st-prime.py
@@ -1,11 +1,17 @@
-marked = []
-prime = []
+import math
 
-while len(prime) < 10002:
-    for i in range (2,1000000):
-        if i not in marked:
-            prime.append(i)
-            for j in range (2, 1000000):
-                marked.append(i*j)
+int_dict = {i: True for i in range(2, 10)}
 
-print(prime[-1])
+## Need to create a constant j variable
+
+for i in range(2, round(math.sqrt(9+1))):
+    if int_dict[i] == True:
+        j = i**2
+        multiplier = 1
+        int_dict[j] = False
+        for j in range(j, 9):
+            j = j+(i*multiplier)
+            int_dict[j] = False
+            multiplier += 1
+
+print(int_dict)
