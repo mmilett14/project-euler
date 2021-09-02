b97d895afed516aa83832b00778d3da6be548006
mike <78767272+mmilett14@users.noreply.github.com>
2021-09-02 14:54:53 -0500

    SO CLOSE!!!

--- 7-10001st-prime.py
+++ 7-10001st-prime.py
@@ -2,18 +2,18 @@ from math import sqrt, ceil, log
 
 # from https://codereview.stackexchange.com/questions/188053/project-euler-problem-7-in-python-10001st-prime-number
 def upper_bound(n):
-if n < 6:
-    return 100
-return ceil(n * (log(n) + log(log(n))))
+    if n < 6:
+        return 100
+    return ceil(n * (log(n) + log(log(n))))
 
 
 def prime(n):
     
-    upper_bound = upper_bound(n)
+    top_range = upper_bound(n)
     
-    int_dict = {i: True for i in range(2, upper_bound+1)}
+    int_dict = {i: True for i in range(2, top_range)}
 
-    for i in range(2, round(sqrt(n))):
+    for i in range(2, round(sqrt(top_range))):
         if int_dict[i] == True:
             j = i**2
             multiplier = 1
@@ -21,16 +21,17 @@ def prime(n):
 
             j_dynamic = j
 
-            while j_dynamic < n-1:
+            while j_dynamic < top_range-1:
                 j_dynamic = j+(i*multiplier)
                 int_dict[j_dynamic] = False
                 multiplier += 1
 
     prime_list = []
     
-    for key, value in int_dict.items():
-        if value == True:
-            prime_list.append(key)
+    while len(prime_list) <= n:
+        for key, value in int_dict.items():
+            if value == True:
+                prime_list.append(key)
             
     print(prime_list[-1])
     
